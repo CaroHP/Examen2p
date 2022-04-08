47757305e3de530a1ad5d2c157fcd381dda223fb
CaroHP <0223297@up.edu.mx>
2022-04-07 21:05:32 -0500

    first commit

---
+++ app/src/main/java/com/lharo/exam/ApplicationAdapter.kt
@@ -0,0 +1,50 @@
+package com.lharo.exam
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ApplicationAdapter(context: Context, feedEntries: ArrayList<MovieModel>): RecyclerView.Adapter<ApplicationAdapter.ViewHolder>() {
+
+    private var localContext: Context? = null
+    private var localFeedEntries: ArrayList<MovieModel>? = null
+
+    init {
+        localContext = context
+        localFeedEntries = feedEntries
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ApplicationAdapter.ViewHolder {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(localContext)//Referencia al MainActivity
+        val view: View = layoutInflater.inflate(R.layout.rssfeedmovies, parent, false)
+        return ViewHolder(view)
+    }
+
+
+
+    override fun onBindViewHolder(holder: ApplicationAdapter.ViewHolder, position: Int) {
+        val currentFeedEntry: MovieModel = localFeedEntries!![position]
+        holder.textile.text = currentFeedEntry.title
+        holder.textrating.text = currentFeedEntry.imdbRating
+        holder.textmeta.text = currentFeedEntry.metascore
+        holder.textvotes.text = currentFeedEntry.imdbVotes
+
+    }
+
+    override fun getItemCount(): Int {
+        return localFeedEntries?.size ?: 0
+    }
+
+    class ViewHolder(v: View): RecyclerView.ViewHolder(v) {
+
+        val textile: TextView = v.findViewById(R.id.textview_pelicula)
+        val textrating: TextView = v.findViewById(R.id.textview_rating)
+        val textmeta: TextView = v.findViewById(R.id.textview_meta)
+        val textvotes: TextView  = v.findViewById(R.id.textview_votes)
+
+    }
+
+}
---
+++ app/src/main/java/com/lharo/exam/MovieModel.kt
@@ -0,0 +1,23 @@
+package com.lharo.exam
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieModel {
+    var title: String = ""
+    var year: String = ""
+    var director: String = ""
+    var genre: String = ""
+    var language: String = ""
+    var metascore: String = ""
+    var imdbRating: String = ""
+    var imdbVotes: String = ""
+    }
+
+
+
